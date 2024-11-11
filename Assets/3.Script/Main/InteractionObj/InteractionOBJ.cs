981732e7ad905063cb83c7b5d21c9dcfce8305a2
최영훈 <thiore94@gmail.com>
2024-11-11 21:35:39 +0900

    [delete] InteractionObj ->ITouchable

--- Assets/3.Script/Main/InteractionObj/InteractionOBJ.cs
+++ Assets/3.Script/Main/InteractionObj/InteractionOBJ.cs
@@ -2,27 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractionOBJ : MonoBehaviour, ITouchable
+public class InteractionOBJ : MonoBehaviour
 {
     private Outline outline;
 
     protected bool isTouching;
 
 
-    public virtual void OnTouchStarted(Vector2 position)
-    {
-        
-
-    }
-    public virtual void OnTouchHold(Vector2 position)
-    {
-        
-    }    
-    public virtual void OnTouchEnd(Vector2 position)
-    {
-
-    }
-
+    
     protected virtual void OnEnable()
     {
         TryGetComponent(out outline);
