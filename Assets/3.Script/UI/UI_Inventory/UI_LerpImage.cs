0ee25919ab2b03a5d4790f37b4b0f15668588b7c
최영훈 <thiore94@gmail.com>
2024-11-14 09:33:08 +0900

    [add]lerpImage

--- Assets/3.Script/UI/UI_Inventory/UI_LerpImage.cs
+++ Assets/3.Script/UI/UI_Inventory/UI_LerpImage.cs
@@ -3,23 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_LerpImage : MonoBehaviour, ITouchable
+public class UI_LerpImage : MonoBehaviour
 {
-    [SerializeField] public RectTransform inventoryButton; // 인벤토리 버튼의 RectTransform
+    [SerializeField]private RectTransform inventoryButton; // 인벤토리 버튼의 RectTransform
+    [SerializeField]private Canvas canvas;
     
+    private Vector2 invenPos;
 
-    public void OnTouchStarted(Vector2 position)
+    private RectTransform lerpImage;
+
+    public Coroutine lerp_co { get; private set; } = null;
+
+    public void Start()
     {
-        throw new System.NotImplementedException();
+        TryGetComponent(out lerpImage);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            (RectTransform)canvas.transform,
+            inventoryButton.anchoredPosition,
+            null,
+            out invenPos);
     }
 
-    public void OnTouchHold(Vector2 position)
+    //아이템의 터치앤드에서 이 함수 불러주시면 됩니다!
+    public void OnLerpItem(Vector2 position)
     {
-        throw new System.NotImplementedException();
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            (RectTransform)canvas.transform,
+            position,
+            null,
+            out Vector2 localPoint);
+
+        if(lerp_co == null)
+        {
+            lerp_co = StartCoroutine(LerpImage_co(localPoint));
+        }
     }
 
-    public void OnTouchEnd(Vector2 position)
+    private IEnumerator LerpImage_co(Vector2 localPoint)
     {
-        throw new System.NotImplementedException();
+        float lerpTime = 0f;
+        while(lerpTime.Equals(1f))
+        {
+            lerpTime += Time.deltaTime;
+            if(lerpTime>1f)
+            {
+                lerpTime = 1f;
+            }
+            lerpImage.anchoredPosition = Vector2.Lerp(localPoint, invenPos, lerpTime);
+            if(!lerpImage.gameObject.activeSelf)
+            {
+                lerpImage.gameObject.SetActive(true);
+            }
+            yield return null;
+        }
+        //여기서 인벤에 들어가는 처리하면 될것같습니다!
+        lerpImage.gameObject.SetActive(false);
+        lerp_co = null;
+        yield break;
+
     }
 }
