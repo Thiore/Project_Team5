836e564bfd1d505684f11ab89fab3ffef132f62b
박재현 <wogus10201289@gmail.com>
2024-11-14 11:45:30 +0900

    [fix] maze wall / ballOBj component seperate

--- Assets/3.Script/mango/Overwrap/OverlapCheck.cs
+++ Assets/3.Script/mango/Overwrap/OverlapCheck.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class OverlapCheck : MonoBehaviour,ITouchable
 {
     public List<OverlapPoint> pointList;
