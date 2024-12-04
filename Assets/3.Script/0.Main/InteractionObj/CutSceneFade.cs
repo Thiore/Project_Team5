a316d99fcb7e245cfc04bc933b6df3c83b029491
백승주 <tmdwn3431@naver.com>
2024-12-04 14:48:55 +0900

    [Update] B2F 사다리 컷씬 프리팹
    모델 -> 프리팹 -> 층별 폴더에 있습니다.

---
+++ Assets/3.Script/0.Main/InteractionObj/CutSceneFade.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneFade : MonoBehaviour
+{
+    public void Fade_In()
+    {
+        LoadingManager.Instance.FadeIn();
+    }
+
+    public void Fade_Out()
+    {
+        LoadingManager.Instance.FadeOut();
+    }
+
+    public void SetPlayerPosition()
+    { 
+
+        Vector3 setPosition = new Vector3(-7.271f, -2.147f, -8.468f);
+        PlayerManager.Instance.mainPlayer.localPosition = setPosition;
+        Debug.Log("여기");
+    }
+}
